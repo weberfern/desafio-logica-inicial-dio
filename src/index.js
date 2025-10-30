2793670c952fffad26246527f1e4a14bcb601580
weberfern <weber12@gmail.com>
2025-10-30 17:25:42 -0300

    Desafio concluido para envio

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-// PARA MANDAR PRO GITHUB:
-// 1 -git add .
-// 2 -git commit -m "mensagem do commit"
-// 3 -git push -u origin main
+// DESAFIO CLASSIFICADOR DE NÍVEL DE HERÓI
 
-console.log("Desafio inicial da DIO")
+let hero = "Super Choque";
+let heroExp = Math.random() * 7550;
+let heroRank = "";
+
+if (heroExp < 1000) {
+    heroRank = "Ferro";
+} else if (heroExp >= 1001 && heroExp <= 2000) {
+    heroRank = "Bronze";
+} else if (heroExp >= 2001 && heroExp <= 5000) {
+    heroRank = "Prata";
+} else if (heroExp >= 5001 && heroExp <= 7000) {
+    heroRank = "Ouro";
+} else if (heroExp >= 7001 && heroExp <= 8000) {
+    heroRank = "Platina";
+} else if (heroExp >= 8001 && heroExp <= 9000) {
+    heroRank = "Ascedente";
+} else if (heroExp >= 9001 && heroExp <= 10000) {
+    heroRank = "Imortal";
+} else if (heroExp >= 10001) {
+    heroRank = "Radiante";
+}
+
+console.log(`O herói de nome ${hero} está no nível de ${heroRank} (${heroExp.toFixed(0)} XP).`);
